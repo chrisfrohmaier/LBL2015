4145222440e408990c419d5a77f236d495037871
Chris Frohmaier <chrisfrohmaier@ip-036-043.vpn.soton.ac.uk>
2015-07-16 15:50:19 -0700

    Histogram code for Ia fits

---
+++ chisq_hist.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+conn = psycopg2.connect(host='srv01050.soton.ac.uk', user='frohmaier', password='rates', database='frohmaier')
+cur = conn.cursor()
+
+cur.execute("SELECT redchi2 from sncosmo_fits where redchi2>0 and redchi2<Inifinty;")
+m=cur.fetchall()
+m=np.array(m)
+
+plt.hist(m[:,0].astype(float), bins=10)
+plt.show()
