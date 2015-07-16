26282ebe4df3e8a73967223ff799280dd48cc1d4
Chris Frohmaier <chrisfrohmaier@ip-036-125.vpn.soton.ac.uk>
2015-07-15 17:10:11 -0700

    Added a 2 pre 2 post check to lightcurve

--- Fit_LC.py
+++ Fit_LC.py
@@ -107,6 +107,9 @@ for i in range( my_nmin, my_nmax):
 		print hml[:,0][0], pass_4cut
 		
 		sncosmo.plot_lc(hml_dat, model=fitted_model, errors=res.errors, color=np.random.choice(flat_cols), figtext=str(hml[:,0][0]), xfigsize=10)
+		plt.title(str(pass_4cut))
+		plt.axvline(pdate-20., color='black', linestyle='--')
+		plt.axvline(pdate+50., color='black', linestyle='--')
 		plt.savefig('LC_Fixed/'+str(hml[:,0][0])+'.png', dpi=150, bbox_inches='tight')
 		plt.close()
 		print '### Parameters ###'
