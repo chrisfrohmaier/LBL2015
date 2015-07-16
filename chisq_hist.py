3f476f02b59a9dae3b01a5a169dd8b2a580a5e2d
Chris Frohmaier <chrisfrohmaier@ip-036-043.vpn.soton.ac.uk>
2015-07-16 16:10:34 -0700

    Changed Save directory

--- chisq_hist.py
+++ chisq_hist.py
@@ -6,10 +6,17 @@ import psycopg2
 conn = psycopg2.connect(host='srv01050.soton.ac.uk', user='frohmaier', password='rates', database='frohmaier')
 cur = conn.cursor()
 
+cur.execute("SELECT redchi2 from sncosmo_fits where redchi2>0 and redchi2<50;")
+n=cur.fetchall()
+n=np.array(n)
+
 cur.execute("SELECT redchi2 from sncosmo_fits where redchi2>0 and redchi2<50 and pass_cut=True;")
 m=cur.fetchall()
 m=np.array(m)
 
-plt.hist(m[:,0].astype(float), bins=100)
+plt.hist(n[:,0].astype(float), bins=100, label='Total Sample')
+plt.hist(m[:,0].astype(float), bins=100, label='Passed Selection')
+plt.xlabel(r'Reduced $\chi^2$')
+plt.ylabel('Number')
 plt.title(r'Reduced $\chi^2$ Fits')
 plt.savefig('../LC_Fitter/Chisq_hist.png', dpi=150)
