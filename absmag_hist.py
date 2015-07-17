5afedc1d450eaa23dfa272b0f62af744a0d48e7f
Chris Frohmaier <chrisfrohmaier@ip-036-043.vpn.soton.ac.uk>
2015-07-17 12:48:10 -0700

    Absolution Magnitude Distribution of Spec Confirmed Ias

--- absmag_hist.py
+++ absmag_hist.py
@@ -17,11 +17,11 @@ m=np.array(m)
 
                                               
 fig = matplotlib.pyplot.gcf()
-fig.set_size_inches(7,5)
+fig.set_size_inches(10,7)
 plt.hist(n[:,0].astype(float), bins=30, label='Total Sample', color='#3498db')
 plt.hist(m[:,0].astype(float), bins=30, label='Passed Selection', color='#7f8c8d')
 plt.xlabel(r'Absolute Magnitude (B Band)')
 plt.ylabel('Number')
 plt.legend()
 plt.title(r'Absolute Magnitude Distribution')
-plt.savefig('../LC_Fitter/abs_mag_hist.png', dpi=150)
+plt.savefig('../LC_Fitter/abs_mag_hist.png', dpi=250)
