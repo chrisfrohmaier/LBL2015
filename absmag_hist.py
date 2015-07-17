abcfeb43fd777c80124e7ea4d8d58250f8ea7c44
Chris Frohmaier <chrisfrohmaier@ip-036-043.vpn.soton.ac.uk>
2015-07-17 12:43:03 -0700

    Absolution Magnitude Distribution of Spec Confirmed Ias

--- absmag_hist.py
+++ absmag_hist.py
@@ -6,11 +6,11 @@ import psycopg2
 conn = psycopg2.connect(host='srv01050.soton.ac.uk', user='frohmaier', password='rates', database='frohmaier')
 cur = conn.cursor()
 
-cur.execute("SELECT abs_mag from sncosmo_fits;")
+cur.execute("SELECT abs_mag from sncosmo_fits where abs_mag>-22;")
 n=cur.fetchall()
 n=np.array(n)
 
-cur.execute("SELECT abs_mag from sncosmo_fits where  pass_cut=True;")
+cur.execute("SELECT abs_mag from sncosmo_fits where  pass_cut=True and abs_mag>-22;")
 m=cur.fetchall()
 m=np.array(m)
 
@@ -18,8 +18,8 @@ fig = plt.figure()
 ax = fig.add_subplot(1,1,1) 
                                               
 
-plt.hist(n[:,0].astype(float), bins=100, label='Total Sample', color='#9b59b6')
-plt.hist(m[:,0].astype(float), bins=100, label='Passed Selection', color='#2ecc71')
+plt.hist(n[:,0].astype(float), bins=30, label='Total Sample', color='#3498db')
+plt.hist(m[:,0].astype(float), bins=30, label='Passed Selection', color='#7f8c8d')
 plt.xlabel(r'Absolute Magnitude (B Band)')
 plt.ylabel('Number')
 plt.legend()
