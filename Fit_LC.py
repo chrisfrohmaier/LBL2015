553cae405c95606f11ce828884071deb9bca22db
Chris Frohmaier <chrisfrohmaier@ip-036-043.vpn.soton.ac.uk>
2015-07-17 11:24:56 -0700

    Abs mag check

--- Fit_LC.py
+++ Fit_LC.py
@@ -122,11 +122,12 @@ for i in range( my_nmin, my_nmax):
 		print 'chi2', sncosmo.chisq(hml_dat, fitted_model)
 		print 'ndof', len(hml_dat)-4. #len(data)-len(vparam_names)
 		print 'red_chi2', sncosmo.chisq(hml_dat, fitted_model)/(len(hml_dat)-4.)
+		print 'absolute magnitue', fitted model.source_peakabsmag('bessellb','ab')
 		# print 'chi2', res.chisq
 		# print 'res.ndof', res.ndof
 		# print 'red_chisq', res.chisq/res.ndof
-		cur.execute("INSERT INTO sncosmo_fits (ptfname, redshift, redshift_err, t0, t0_err, x0, x0_err, x1, x1_err, c, c_err, ra, dec, pass_cut, redchi2) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s);",(str(hml[:,0][0]), float(zed[0]), float(0), float(res.parameters[1]), float(res.errors['t0']),float(res.parameters[2]), float(res.errors['x0']),  float(res.parameters[3]), float(res.errors['x1']), float(res.parameters[4]), float(res.errors['c']), float(hml[:,8][0]), float(hml[:,9][0]), pass_4cut,float(sncosmo.chisq(hml_dat, fitted_model)/(len(hml_dat)-4.)),))
-		conn.commit()
+		#cur.execute("INSERT INTO sncosmo_fits (ptfname, redshift, redshift_err, t0, t0_err, x0, x0_err, x1, x1_err, c, c_err, ra, dec, pass_cut, redchi2) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s);",(str(hml[:,0][0]), float(zed[0]), float(0), float(res.parameters[1]), float(res.errors['t0']),float(res.parameters[2]), float(res.errors['x0']),  float(res.parameters[3]), float(res.errors['x1']), float(res.parameters[4]), float(res.errors['c']), float(hml[:,8][0]), float(hml[:,9][0]), pass_4cut,float(sncosmo.chisq(hml_dat, fitted_model)/(len(hml_dat)-4.)),))
+		#conn.commit()
 		print 'Done:', hml[:,0][0]
 		
 	except ValueError:
