cde88536d2748527277c459fbc14d17f263d0d30
Chris Frohmaier <chrisfrohmaier@ip-036-099.vpn.soton.ac.uk>
2015-07-21 11:16:29 -0700

    Comparing the input Supernovae to the Output

--- inout_compare.py
+++ inout_compare.py
@@ -56,3 +56,11 @@ fig.set_size_inches(15,12)
 
 plt.savefig('../LC_Fitter/Sim_LC_Fit/Diff_Hist.png', dpi=300, bbox_inches='tight')
 plt.close()
+
+
+plt.title(r'Reduced \chi^2 Distribution for Fit to Simulated Lightcurves')
+plt.hist(m[:,9], bins=200, color=flat_cols[3],histtype='stepfilled', edgecolor = "none" )
+fig = matplotlib.pyplot.gcf()
+fig.set_size_inches(15,12)
+plt.savefig('../LC_Fitter/Sim_LC_Fit/inout_redchi2.png', dpi=300, bbox_inches='tight')
+plt.close
