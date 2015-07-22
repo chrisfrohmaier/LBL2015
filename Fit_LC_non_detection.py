43873e837df12472a5aa531248508c1062127023
Chris Frohmaier <chrisfrohmaier@ip-036-099.vpn.soton.ac.uk>
2015-07-21 17:10:01 -0700

    Included Non detections

--- Fit_LC_non_detection.py
+++ Fit_LC_non_detection.py
@@ -97,7 +97,7 @@ for i in range( my_nmin, my_nmax):
 		#res, fitted_model=sncosmo.fit_lc(hml_dat, model, ['t0','x0','x1','c'], bounds={'x1':(-3.5,3.5), 'c':(-0.35,0.45)}, verbose=True)
 		#res, fitted_model=sncosmo.nest_lc(hml_dat, model, ['t0','x0','x1','c'], bounds={'x1':(-3.5,3.5), 'c':(-0.35,0.45)},)
 		pdate=res.parameters[1]
-		pass_4cut=Check_Dates(hml, pdate)
+		pass_4cut=Check_Dates(hml_dat, pdate)
 		#print l[i], pass_4cut
 		
 
