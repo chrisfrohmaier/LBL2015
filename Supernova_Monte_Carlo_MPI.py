e7fe3f8de4029df344d7961472bb440e7d3f37bd
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-08-17 10:24:50 +0100

    Added NERSC deep_ref criteria, changed the date range in the main loop

--- Supernova_Monte_Carlo_MPI.py
+++ Supernova_Monte_Carlo_MPI.py
@@ -80,9 +80,74 @@ def Get_Fields_Max_Area_2010(ujd_start, ujd_stop):
 	#plt.savefig('The_2010_Sky.png', dpi=300, bbox_inches='tight')
 	plt.close()	
 	'''
+
+def ccd0_defect(com, alpha):
+	bad_area=[]
+	xt=com[0]
+	yt=com[1]
+
+	jonest=np.matrix([[np.cos(alpha), -np.sin(alpha), xt], [np.sin(alpha), np.cos(alpha), yt], [0,0,1.]])
+	good_area=[]
+
+	xl_scale=abs(com[0]-com[2])/((2048*1.01)/3600)
+	xr_scale=abs(com[4]-com[6])/((2048*1.01)/3600)
+	
+	
+	for i in [(147.*xl_scale,1501.), (177.*xl_scale,1501.),(177.*xr_scale,4097.),(147.*xr_scale,4097.)]:
+		x=i[0]*(1.01/3600)
+		y=i[1]*(1.01/3600)
+		pixel_coords=np.matrix([[x],[y],[1.]])
+		#print 'Pixel:', pixel_coords
+		cords=np.dot(jonest,pixel_coords)
+		#print 'New Coordinates:', cords
+		#print cords.item(0)
+		bad_area.append((cords.item(0),yt-(cords.item(1)-yt)))
+	return Polygon(bad_area)
+
+def all_CCD_check(com, ra, dec):	
+	sima=Polygon([(com[0],com[1]),(com[2],com[3]),(com[4],com[5]),(com[6],com[7])])
+	#simp = PolygonPatch(sima, facecolor='k',zorder=0)
+	#plt.gca().add_patch(simp)
+	alpha=np.arctan(np.divide((np.subtract(com[6],com[0])),np.subtract(com[7],com[1])))
+	#print alpha
+
+	xt=com[0]
+	yt=com[1]
+
+	jonest=np.matrix([[np.cos(alpha), -np.sin(alpha), xt], [np.sin(alpha), np.cos(alpha), yt], [0,0,1.]])
+	#print jonest
+
+	good_area=[]
+
+	xl_scale=abs(com[0]-com[2])/((2048*1.01)/3600)
+	xr_scale=abs(com[4]-com[6])/((2048*1.01)/3600)
+	
+	yu_scale=abs(com[3]-com[5])/((4096*1.01)/3600)
+	
+	yl_scale=abs(com[1]-com[7])/((4096*1.01)/3600)
+
+	for i in [(75.*xl_scale,75.), (2048.*xl_scale,75.),(2048.*xr_scale,4000.),(75.*xr_scale,4000.)]:
+		x=i[0]*(1.01/3600)
+		y=i[1]*(1.01/3600)
+		pixel_coords=np.matrix([[x],[y],[1.]])
+		#print 'Pixel:', pixel_coords
+		cords=np.dot(jonest,pixel_coords)
+		#print 'New Coordinates:', cords
+		#print cords.item(0)
+		good_area.append((cords.item(0),yt-(cords.item(1)-yt)))
+	
+	garea=[good_area[0], (good_area[1][0],com[3]-((75.*1.01/3600)*yu_scale)), (good_area[2][0],com[5]+((96.*1.01/3600)*yu_scale)),good_area[3]]
+	#print good_area
+	#print [(com[0],com[1]),(com[2],com[3]),(com[4],com[5]),(com[6],com[7])]
+	simg1=Polygon(garea)
+	if com[8]==0:
+		simb0=ccd0_defect(com, alpha)
+		simg1=simg1.difference(simb0)
+	return simg1.contains(Point(ra,dec))
+
 def Get_Obs_Conditions(Ra, Dec, peak_date, cur=cur):
 	
-	cur.execute("SELECT geo_sub.ujd, geo_sub.ptffield, geo_sub.ccdid, geo_sub.lmt_mg_new, (geo_sub.seeing_new/geo_sub.seeing_ref), geo_sub.medsky_new, geo_sub.good_pix_area, ptffield.color_excess FROM geo_sub JOIN ptffield ON geo_sub.ptffield=ptffield.id WHERE geo_sub.filter='R' and ujd>(%s-20.) and ujd<(%s+50.) and height<10 and width<10 and st_contains(geo_sub.geo_ccd, ST_GeometryFromText('Point(%s %s)',4326))=True  ORDER BY geo_sub.ujd ASC;",(float(peak_date),float(peak_date),float(Ra),float(Dec),)) #Get Everything from the Subtraction Table
+	cur.execute("SELECT geo_sub.ujd, geo_sub.ptffield, geo_sub.ccdid, geo_sub.lmt_mg_new, (geo_sub.seeing_new/geo_sub.seeing_ref), geo_sub.medsky_new, geo_sub.good_pix_area, ptffield.color_excess, geo_sub.ra_ll,geo_sub.dec_ll,geo_sub.ra_ul,geo_sub.dec_ul,geo_sub.ra_ur,geo_sub.dec_ur,geo_sub.ra_lr,geo_sub.dec_lr FROM geo_sub JOIN ptffield ON geo_sub.ptffield=ptffield.id WHERE geo_sub.filter='R' and ujd>(%s-25.) and ujd<(%s+55.) and height<10 and width<10 and geo_sub.deep_ref_id<=17783 and st_contains(geo_sub.geo_ccd, ST_GeometryFromText('Point(%s %s)',4326))=True  ORDER BY geo_sub.ujd ASC;",(float(peak_date),float(peak_date),float(Ra),float(Dec),)) #Get Everything from the Subtraction Table
 	m=cur.fetchall()
 	
 	m=np.array(m)
@@ -95,9 +160,30 @@ def Get_Obs_Conditions(Ra, Dec, peak_date, cur=cur):
 	m[:,5]	| medsky_new
 	m[:,6]	| good_pix_area	
 	m[:,7]	| color_excess
+	m[:,8]	| ra_ll
+	m[:,9]	| dec_ll
+	m[:,10]	| ra_ul
+	m[:,11]	| dec_ul
+	m[:,12]	| ra_ur
+	m[:,13]	| dec_ur
+	m[:,14]	| ra_lr
+	m[:,15]	| dec_lr
 	'''
-	
-	return m
+	#print 'mlen', len(m[:,0])
+	#print m
+	#print len(m[:])
+	if len(m[:]) != 0:
+		#com1=[m[:,8],m[:,9],m[:,10],m[:,11],m[:,12],m[:,13],m[:,14],m[:,15],m[:,16],m[:,17],m[:,2]]
+		in_ccd=[]
+		for i in range(0,len(m[:,0])):
+			#print i
+			com=[m[:,8][i],m[:,9][i],m[:,10][i],m[:,11][i],m[:,12][i],m[:,13][i],m[:,14][i],m[:,15][i],m[:,2][i]]
+			in_ccd.append(all_CCD_check(com, Ra, Dec))
+		m=np.column_stack((m,in_ccd)) #Now m[:,16] is a true/false column of whether object is in the good region
+		#print m
+		return m[m[:,16]==True]	
+	else:
+		return m
 def Remap_Ranges(care, rang):
 	return (care-min(rang))*(len(rang))/((max(rang)-min(rang)))
 
@@ -108,6 +194,7 @@ def Interp_On_Zoom(mag, lmt, med, see, zoom_grid=zoom_grid, Mag_Zoom=Mag_Zoom, L
 def Pass_Selection(sn_arr, find_bool, peak_date):
 	'''
 	'''
+
 	low=False
 	high=False
 	#print 'sn_arr', sn_arr[:,0]
@@ -139,8 +226,7 @@ def Check_Detection(sn_par):
 	'''
 	probs=(Interp_On_Zoom(sn_par[:,1], sn_par[:,4], sn_par[:,6], sn_par[:,5])).T
 
-	'''The line tot probs is not needed because the original efficiency grid took care of this
-	tot_prob=(np.multiply(probs[:,0],np.divide(sn_par[:,7],0.6603)))'''
+	tot_prob=probs[:,0]
 	#print 'Prob',probs[:,0]
 	#print 'Tot_Prob', tot_prob
 	ran=np.random.uniform(0,1,len(tot_prob))
@@ -188,10 +274,10 @@ def Random_sneParameters(low_ujd, high_ujd, low_ra, high_ra, low_dec, high_dec):
 
 def Gen_SN(peak_date, Ra, Dec, redshift, colour,x_1, int_dis, cur=cur):
 	#Use below if on Iridis
-	source = sncosmo.SALT2Source(modeldir="/scratch/cf5g09/Monte_Carlos/salt2-4")
+	#source = sncosmo.SALT2Source(modeldir="/scratch/cf5g09/Monte_Carlos/salt2-4")
 
 	##Use below if not on iridis
-	#source=sncosmo.get_source('salt2',version='2.4') 
+	source=sncosmo.get_source('salt2',version='2.4') 
 	
 
 	alpha=0.141
@@ -236,6 +322,9 @@ def Gen_SN(peak_date, Ra, Dec, redshift, colour,x_1, int_dis, cur=cur):
 	#print maglc
 	#print mag_lc
 	sn_par=np.array((time_array, mag_lc, flux_lc, ccd_lc, lmt_lc, see_rat, med_lc, pix_lc )).T
+	if sn_par.size == 0:
+		return mabs, 9999.99, False, 9999.99
+
 	'''snapr
 	snpar[:,0]	| time
 	snpar[:,1]	| mag_lc
@@ -254,7 +343,8 @@ def update_sn_mc_table(peak_date, ra, dec, ab_magb, redshift, x1, color, int_dis
 	cur2.execute("INSERT INTO sn_mc (peak_date, ra, dec, ab_magb, redshift, x1, color, int_dis, found, ebv) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s, %s)",(float(peak_date), ra, dec, ab_magb, redshift, x1, color, int_dis, found, ebv))
 	conn2.commit()
 
-N_MODELS_TOTAL = 50000000
+
+N_MODELS_TOTAL = 500
 ra_array=np.ones(N_MODELS_TOTAL)
 dec_array=np.ones(N_MODELS_TOTAL)
 found_array=np.ones(N_MODELS_TOTAL)
@@ -289,16 +379,17 @@ ndo = my_nmax - my_nmin
 #print 'Time', TI.time()*(my_rank+1*np.pi)
 
 for i in range( my_nmin, my_nmax):
-	Ra, Dec, peak_date, xone, color, zedshift, int_dis=Random_sneParameters(2455317.5,2455500.5,310,360,-7.,20.)
+	Ra, Dec, peak_date, xone, color, zedshift, int_dis=Random_sneParameters(2455256.5,2455500.5,310,360,-7.,20.)
 
 	absmagb, absmag_r, sn_par, ebv=Gen_SN(peak_date, Ra, Dec, zedshift, color,xone, int_dis, cur=cur)
+	#print sn_par
 	if absmag_r==9999.99:
 		Pass=False
-	elif absmag_r<9999.99:
+	elif absmag_r<9999.99:		
 		find_bool=Check_Detection(sn_par)
-		Pass=Pass_Selection(sn_par, find_bool, peak_date)
+		Pass=Pass_Selection(sn_par, find_bool[:,0], peak_date)
 	#print Ra, Dec, Pass
-	update_sn_mc_table(peak_date, Ra, Dec, absmagb, zedshift, xone, color, int_dis, Pass, ebv)
+	#update_sn_mc_table(peak_date, Ra, Dec, absmagb, zedshift, xone, color, int_dis, Pass, ebv)
 
 #uncomment this at the end of your script
 cur.close()
