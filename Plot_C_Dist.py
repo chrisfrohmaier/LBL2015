8c423f1c792d269771e40c1fd61248eaf9375f6a
chrisfrohmaier <cf5g09@soton.ac.uk>
2015-09-22 14:45:00 +0100

    200 bins

--- Plot_C_Dist.py
+++ Plot_C_Dist.py
@@ -41,7 +41,7 @@ flat_cols=['#1abc9c','#2ecc71','#3498db','#9b59b6','#34495e','#f39c12','#d35400'
 
 colour=m[:,0]
 #mag_bin=np.linspace(min(mag),max(mag),20)
-hist,bine=np.histogram(m[:,0], bins=100)
+hist,bine=np.histogram(m[:,0], bins=200)
 print bine
 print hist
 #, color=flat_cols[1], label='Simulated Sample Distribution', normed=True
