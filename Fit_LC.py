65e85aaea157f60b4d5155caa8341f4e13065c34
chrisfrohmaier <c.frohmaier@soton.ac.uk>
2015-09-23 12:31:17 +0100

    Added ptf48r to the absolute magnitude

--- Fit_LC.py
+++ Fit_LC.py
@@ -127,11 +127,11 @@ for i in range( my_nmin, my_nmax):
 		print 'chi2', sncosmo.chisq(hml_dat, fitted_model)
 		print 'ndof', len(hml_dat)-4. #len(data)-len(vparam_names)
 		print 'red_chi2', sncosmo.chisq(hml_dat, fitted_model)/(len(hml_dat)-4.)
-		print 'absolute magnitue', fitted_model.source_peakabsmag('bessellb','ab')
+		print 'absolute magnitue', fitted_model.source_peakabsmag('ptf48r','ab')
 		# print 'chi2', res.chisq
 		# print 'res.ndof', res.ndof
 		# print 'red_chisq', res.chisq/res.ndof
-		cur.execute("INSERT INTO sncosmo_fits (ptfname, redshift, redshift_err, t0, t0_err, x0, x0_err, x1, x1_err, c, c_err, ra, dec, pass_cut, redchi2, abs_mag) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",(str(hml[:,0][0]), float(zed[0]), float(0), float(res.parameters[1]), float(res.errors['t0']),float(res.parameters[2]), float(res.errors['x0']),  float(res.parameters[3]), float(res.errors['x1']), float(res.parameters[4]), float(res.errors['c']), float(hml[:,8][0]), float(hml[:,9][0]), pass_4cut,float(sncosmo.chisq(hml_dat, fitted_model)/(len(hml_dat)-4.)), float(fitted_model.source_peakabsmag('bessellb','ab')),))
+		cur.execute("INSERT INTO sncosmo_fits (ptfname, redshift, redshift_err, t0, t0_err, x0, x0_err, x1, x1_err, c, c_err, ra, dec, pass_cut, redchi2, abs_mag) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",(str(hml[:,0][0]), float(zed[0]), float(0), float(res.parameters[1]), float(res.errors['t0']),float(res.parameters[2]), float(res.errors['x0']),  float(res.parameters[3]), float(res.errors['x1']), float(res.parameters[4]), float(res.errors['c']), float(hml[:,8][0]), float(hml[:,9][0]), pass_4cut,float(sncosmo.chisq(hml_dat, fitted_model)/(len(hml_dat)-4.)), float(fitted_model.source_peakabsmag('ptf48r','ab')),))
 		conn.commit()
 		print 'Done:', hml[:,0][0]
 		
